e096d2539da739a69bfcd3c42fac9db2a4d4ab09
Roman Lesnoy <romanlesnoy@outlook.com>
2021-12-02 19:45:06 +0300

    Format with Prettier

--- data/day-01/read-file.js
+++ data/day-01/read-file.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports.readFile = () => {
-    const filePath = path.join(__dirname, 'input.txt')
-    const inputData = fs.readFileSync(filePath, { encoding: 'utf8' })
+    const filePath = path.join(__dirname, 'input.txt');
+    const inputData = fs.readFileSync(filePath, { encoding: 'utf8' });
     return inputData;
 };
--- src/day-01.js
+++ src/day-01.js
@@ -1,12 +1,15 @@
 const { readFile } = require('../data/day-01/read-file');
 
-const data = readFile().trim().split('\n').map(value => parseInt(value, 10));
+const data = readFile()
+    .trim()
+    .split('\n')
+    .map((value) => parseInt(value, 10));
 
 const countIncreasedValue = (values) => {
     let counter = 0;
     for (let i = 0; i < values.length; i++) {
-        if (values[i] < values[i+1]) {
-            counter++
+        if (values[i] < values[i + 1]) {
+            counter++;
         }
     }
     return counter;
@@ -14,16 +17,15 @@ const countIncreasedValue = (values) => {
 
 const sumSlidingWindow = (data) => {
     const sumsArray = [];
-
-    for(let i = 2; i < data.length; i++) {
-        const sum = data[i-2] + data[i-1] + data[i];
+    for (let i = 2; i < data.length; i++) {
+        const sum = data[i - 2] + data[i - 1] + data[i];
         sumsArray.push(sum);
     }
     return sumsArray;
-}
+};
 
 const part1 = countIncreasedValue(data); //1754
-const part2 = countIncreasedValue(sumSlidingWindow(data));//1789
+const part2 = countIncreasedValue(sumSlidingWindow(data)); //1789
 
 console.log(part1);
 console.log(part2);
